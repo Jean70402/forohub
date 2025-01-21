3593ba90d97753ea493a66e497a80bf978be73d8
Jean70402 <jean7040404@gmail.com>
2025-01-21 15:43:05 -0500

    Configuracion inicial

---
+++ src/main/java/com/levelblock/forohub/ForohubApplication.java
@@ -0,0 +1,13 @@
+package com.levelblock.forohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForohubApplication.class, args);
+	}
+
+}
